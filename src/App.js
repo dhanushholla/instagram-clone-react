12b1e19da52e9ce3184c3e53a8857c806b0cffbd
Dhanush Holla <dhanushholla.mdh@gmail.com>
2022-09-15 13:01:36 +0530

    Design css changes done - refactoring

--- src/App.js
+++ src/App.js
@@ -1,76 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-import FormPage from './components/Formpage';
-import Post from './components/post';
-import React, { Component } from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-  Navigate
-} from "react-router-dom";
-import Mainpage from './components/Mainpage';
-
+import logo from "./logo.svg";
+import "./App.css";
+import FormPage from "./components/Formpage";
+import Post from "./components/post";
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+import Mainpage from "./components/Mainpage";
 
 class App extends Component {
   constructor(props) {
-    super(props)
-  
+    super(props);
+
     this.state = {
-      loginname:"",//going to store currenlty logged person cred
-      isloggedin:false,
-      usercreds:[{logname:'test',pass:'test'}]
-    }
+      loginname: "", //going to store currenlty logged person cred
+      isloggedin: false,
+      usercreds: [{ logname: "test", pass: "test" }],
+    };
   }
 
-
-  logininfo=(a)=>{
-    this.setState({isloggedin:true,loginname:a})
+  logininfo = (a) => {
+    this.setState({ isloggedin: true, loginname: a });
     // this.setState({loginname:a})
+  };
 
-  }
-
-  handleregistration=(a,b,e)=>{
+  handleregistration = (a, b, e) => {
     // e.preventDefault();
-    console.log(a,b);
+    console.log(a, b);
     this.setState({
       // loginname:[...this.state.loginname,a],
       // passwd:[...this.state.passwd,b],
-      usercreds:[...this.state.usercreds,{logname:a,pass:b}],
-      
-    })
-  }
-   
+      usercreds: [...this.state.usercreds, { logname: a, pass: b }],
+    });
+  };
+
   render() {
-    return (<>
-       <div className='App' id="app">
-        <Routes>
-          <Route path="/" element={<FormPage loginname={this.state.loginname} passwd={this.state.passwd} loginfn={this.logininfo} sendregisterdata={this.handleregistration} usercreds={this.state.usercreds}/>}> 
-          </Route>
-          {  this.state.isloggedin &&
-          <Route path="/feed" element = {<Mainpage loginname={this.state.loginname}/>} >
-          </Route>
-          } 
-          <Route path="*" element={<Navigate to="/" replace></Navigate>}></Route>
-          {/* the above route seeks if there is no suitable path(*) available for the given request,then the route request is replaced and navigated to our preferred page ie here / ==> formpage */}
-        </Routes>
-        </div> 
-        </>
-  );
+    return (
+      <>
+        <div className="App" id="app">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <FormPage
+                  loginname={this.state.loginname}
+                  passwd={this.state.passwd}
+                  loginfn={this.logininfo}
+                  sendregisterdata={this.handleregistration}
+                  usercreds={this.state.usercreds}
+                />
+              }
+            ></Route>
+            {this.state.isloggedin && (
+              <Route
+                path="/feed"
+                element={<Mainpage loginname={this.state.loginname} />}
+              ></Route>
+            )}
+            <Route
+              path="*"
+              element={<Navigate to="/" replace></Navigate>}
+            ></Route>
+            {/* the above route seeks if there is no suitable path(*) available for the given request,then the route request is replaced and navigated to our preferred page ie here / ==> formpage */}
+          </Routes>
+        </div>
+      </>
+    );
+  }
+  // render() {
+  //   return (
+  //     <div className='App' id="app">
+  //       Instagram clone page
+
+  //     <FormPage></FormPage>
+  //     <Modal></Modal>
+  //     </div>
+  //   )
+  // }
 }
-// render() {
-//   return (
-//     <div className='App' id="app">
-//       Instagram clone page
-
-//     <FormPage></FormPage>
-//     <Modal></Modal>
-//     </div>
-//   )
-// }
-}
-
-
 
 export default App;
--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -1,138 +1,170 @@
-import React, { Component } from 'react'
-import Post from './post'
-import './Mainpage.css'
-import {Link} from 'react-router-dom'
-import {FaHome, FaSearch, FaShoppingBag} from 'react-icons/fa'
-import {BsPersonCircle} from 'react-icons/bs'
-import {BiMoviePlay} from 'react-icons/bi'
-import {toast, ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
-let newcaption="",newimage="",newcomment=[""]
+import React, { Component } from "react";
+import Post from "./post";
+import "./Mainpage.css";
+import { Link } from "react-router-dom";
+import { FaHome, FaSearch, FaShoppingBag } from "react-icons/fa";
+import { BsPersonCircle } from "react-icons/bs";
+import { BiMoviePlay } from "react-icons/bi";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+let newcaption = "",
+  newimage = "",
+  newcomment = [""];
 class Mainpage extends Component {
   constructor(props) {
     super(props);
     // console.log("main page constructor")
-  this.state = {
-      showform:false,
-    postdatas:[
-    {
-      profilepic:"https://images.unsplash.com/photo-1529911194209-8578109840df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFkeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-      username:"Vani",
-      caption:"thala vazhlikuthu da thala valikudhae",
-      mainimage:"https://c.tenor.com/MuhlnnRpq2wAAAAM/vadivelu-thalavalikudu-da.gif",
-    
-    },
-    {
-      profilepic:"https://images.unsplash.com/photo-1606663889134-b1dedb5ed8b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-      username:"Holla",
-      caption:"How do i tell u 😂",
-      mainimage:"https://c.tenor.com/_Hj95ZMsq8cAAAAC/how-do-i-tell-you-vadivelu.gif",
-    },
-    {
-      profilepic:"https://images.unsplash.com/photo-1610276198568-eb6d0ff53e48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cG90cmFpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60", 
-      username:"Anu",
-      caption:"inimae instagram na indha design dhaana da nyabagam varum deii 😂🤣",
-      mainimage:"https://c.tenor.com/a98TfgzHHc8AAAAM/vadivelu-singamuthu.gif",
-     
-    } 
-  ]
+    this.state = {
+      showform: false,
+      postdatas: [
+        {
+          profilepic:
+            "https://images.unsplash.com/photo-1529911194209-8578109840df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFkeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+          username: "Vani",
+          caption: "thala vazhlikuthu da thala valikudhae",
+          mainimage:
+            "https://c.tenor.com/MuhlnnRpq2wAAAAM/vadivelu-thalavalikudu-da.gif",
+        },
+        {
+          profilepic:
+            "https://images.unsplash.com/photo-1606663889134-b1dedb5ed8b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+          username: "Holla",
+          caption: "How do i tell u 😂",
+          mainimage:
+            "https://c.tenor.com/_Hj95ZMsq8cAAAAC/how-do-i-tell-you-vadivelu.gif",
+        },
+        {
+          profilepic:
+            "https://images.unsplash.com/photo-1610276198568-eb6d0ff53e48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cG90cmFpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+          username: "Anu",
+          caption:
+            "inimae instagram na indha design dhaana da nyabagam varum deii 😂🤣",
+          mainimage:
+            "https://c.tenor.com/a98TfgzHHc8AAAAM/vadivelu-singamuthu.gif",
+        },
+      ],
+    };
   }
-}
 
-    handleAddpost =()=>{
-        this.setState({
-            showform:true,
-        }
-      )
-    }
-    handleClosepost =(e)=>{
-      this.setState({
-        showform:false
-      })
-    }
-    handlenewCaption =(e)=>{
-        newcaption=e.target.value;
-    }
-    handlenewImage = (e)=>{
-      newimage=URL.createObjectURL(e.target.files[0])
-    }
+  handleAddpost = () => {
+    this.setState({
+      showform: true,
+    });
+  };
+  handleClosepost = (e) => {
+    this.setState({
+      showform: false,
+    });
+  };
+  handlenewCaption = (e) => {
+    newcaption = e.target.value;
+  };
+  handlenewImage = (e) => {
+    newimage = URL.createObjectURL(e.target.files[0]);
+  };
 
-    handlenewpost =(e)=>{
-      e.preventDefault();
-      console.log("state is changed");
-      this.setState({       
-        // postdatas:[{profilepic:"https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FydG9vbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",username:"tester",caption:newcaption,mainimage:newimage,comments:newcomment},...this.state.postdatas,],
-        postdatas:[...this.state.postdatas,{profilepic:"https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FydG9vbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",username:this.props.loginname,caption:newcaption,mainimage:newimage,comments:newcomment}], 
-        showform:false
-      })
-      newimage=""
-      newcaption=""
-      newcomment=""
-      window.scrollTo(0,0);
-      toast.success('new-post added!', {
-        position: "top-right",
-        autoClose: 1000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: true,
-        progress: undefined,
-        });
-
-    } 
+  handlenewpost = (e) => {
+    e.preventDefault();
+    console.log("state is changed");
+    this.setState({
+      // postdatas:[{profilepic:"https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FydG9vbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",username:"tester",caption:newcaption,mainimage:newimage,comments:newcomment},...this.state.postdatas,],
+      postdatas: [
+        ...this.state.postdatas,
+        {
+          profilepic:
+            "https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FydG9vbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+          username: this.props.loginname,
+          caption: newcaption,
+          mainimage: newimage,
+          comments: newcomment,
+        },
+      ],
+      showform: false,
+    });
+    newimage = "";
+    newcaption = "";
+    newcomment = "";
+    window.scrollTo(0, 0);
+    toast.success("new-post added!", {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+    });
+  };
   render() {
     // console.log("mainpage render")
     return (
-    <React.Fragment>
-      
-      <div className='result'>
-      <nav className='navbar'>
-      <img src="https://www.instagram.com/static/images/web/logged_out_wordmark-2x.png/d2529dbef8ed.png" alt="logo"></img>
-      <button onClick={this.handleAddpost} className="addpostbtn">add post</button>
-      <Link to="/" className='Linkcls'><button className='logoutbtn'>Logout</button></Link>
-      </nav>
-      {
-        this.state.showform &&
-        (
-            <div className="new-form" >
-                <form>
+      <React.Fragment>
+        <div className="result">
+          <nav className="navbar">
+            <img
+              src="https://www.instagram.com/static/images/web/logged_out_wordmark-2x.png/d2529dbef8ed.png"
+              alt="logo"
+            ></img>
+            <button onClick={this.handleAddpost} className="addpostbtn">
+              add post
+            </button>
+            <Link to="/" className="Linkcls">
+              <button className="logoutbtn">Logout</button>
+            </Link>
+          </nav>
+          {this.state.showform && (
+            <div className="new-form">
+              <form>
                 <div>
-                <label>Newpost-Caption:</label>
-                  <input type="text" placeholder="enter caption" onChange={this.handlenewCaption} autoFocus required/>
-                  </div>
-                  <div className='new-imageupload'>
+                  <label>Newpost-Caption:</label>
+                  <input
+                    type="text"
+                    placeholder="enter caption"
+                    onChange={this.handlenewCaption}
+                    autoFocus
+                    required
+                  />
+                </div>
+                <div className="new-imageupload">
                   <label>NewImage:</label>
-                  <input type="file" onChange={this.handlenewImage} required></input>
-                  </div>
-                  <div>
-                  <button type='submit' onClick={this.handlenewpost}>upload post</button>
+                  <input
+                    type="file"
+                    onChange={this.handlenewImage}
+                    required
+                  ></input>
+                </div>
+                <div>
+                  <button type="submit" onClick={this.handlenewpost}>
+                    upload post
+                  </button>
                   <button onClick={this.handleClosepost}>close form</button>
-                  </div>
-                </form>
+                </div>
+              </form>
             </div>
-        )
-      }
-      <div className='feedposts'>
-       { this.state.postdatas.map(
-        (post)=>(<Post name={post.username} image={post.mainimage} caption={post.caption} profilepic={post.profilepic} loginname={this.props.loginname}></Post>)
-        )
-       }
-      </div>
-      <ToastContainer/>
-      <nav className='footer-navbar'>
-       <FaHome size='1.5rem' ></FaHome>
-       <FaSearch size='1.5rem'></FaSearch>
-       <BiMoviePlay size='1.5rem'></BiMoviePlay>
-       <FaShoppingBag size='1.5rem'/>
-       <BsPersonCircle size='1.5rem'/>
-
-      </nav>
-      </div>
-      
+          )}
+          <div className="feedposts">
+            {this.state.postdatas.map((post) => (
+              <Post
+                name={post.username}
+                image={post.mainimage}
+                caption={post.caption}
+                profilepic={post.profilepic}
+                loginname={this.props.loginname}
+              ></Post>
+            ))}
+          </div>
+          <ToastContainer />
+          <nav className="footer-navbar">
+            <FaHome size="1.5rem"></FaHome>
+            <FaSearch size="1.5rem"></FaSearch>
+            <BiMoviePlay size="1.5rem"></BiMoviePlay>
+            <FaShoppingBag size="1.5rem" />
+            <BsPersonCircle size="1.5rem" />
+          </nav>
+        </div>
       </React.Fragment>
-
-    )
+    );
   }
 }
 
-export default Mainpage
+export default Mainpage;
